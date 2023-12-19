bb4f290cc063f7de5a9fa46fde82d36991b39fa0
amitiwary999 <tiwarya9@gmail.com>
2023-12-19 22:34:41 +0530

    own impl to stop the duplicate call and save resource

--- noduplicate/singlecall.go
+++ noduplicate/singlecall.go
@@ -30,10 +30,17 @@ func (g *Group) Do(key string, fn func() (interface{}, error)) (v interface{}, e
 	c.wg.Add(1)
 	g.m[key] = c
 	g.mu.Unlock()
-
+	g.makeCall(c, key, fn)
 	return c.val, c.err
 }
 
 func (g *Group) makeCall(c *call, key string, fn func() (interface{}, error)) {
+	defer func() {
+		c.wg.Done()
+		if g.m[key] != nil {
+			delete(g.m, key)
+		}
+	}()
 
+	c.val, c.err = fn()
 }
