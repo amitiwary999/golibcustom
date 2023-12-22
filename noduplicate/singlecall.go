7ca0311e056df7c87cd53cf201914011e71b64e5
amitiwary999 <tiwarya9@gmail.com>
2023-12-22 23:36:51 +0530

    save the stack trace of error and add code comment

--- noduplicate/singlecall.go
+++ noduplicate/singlecall.go
@@ -1,8 +1,10 @@
 package noduplicate
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"runtime/debug"
 	"sync"
 )
 
@@ -10,6 +12,7 @@ var errGoexit = errors.New("runtime.Goexit was called")
 
 type panicError struct {
 	value interface{}
+	stack []byte
 }
 
 func (p *panicError) Error() string {
@@ -17,7 +20,15 @@ func (p *panicError) Error() string {
 }
 
 func newPanicError(v interface{}) error {
-	return &panicError{value: v}
+	stack := debug.Stack()
+
+	// The first line of the stack trace is of the form "goroutine N [status]:"
+	// but by the time the panic reaches Do the goroutine may no longer exist
+	// and its status will have changed. Trim out the misleading line.
+	if line := bytes.IndexByte(stack[:], '\n'); line >= 0 {
+		stack = stack[line+1:]
+	}
+	return &panicError{value: v, stack: stack}
 }
 
 type call struct {
@@ -56,6 +67,8 @@ func (g *Group) makeCall(c *call, key string, fn func() (interface{}, error)) {
 	normalError := false
 	errorRecovered := false
 	defer func() {
+		/** if fn do runtime.Goexist then it means that normalError and errorRecovered remain false
+		and not changed by function at the end below */
 		if !normalError && !errorRecovered {
 			c.err = errGoexit
 		}
@@ -70,6 +83,7 @@ func (g *Group) makeCall(c *call, key string, fn func() (interface{}, error)) {
 
 	func() {
 		defer func() {
+			/** normalError false means that some error happens on fn() below and it return from there and not move to next line */
 			if !normalError {
 				if r := recover(); r != nil {
 					c.err = newPanicError(r)
@@ -79,7 +93,8 @@ func (g *Group) makeCall(c *call, key string, fn func() (interface{}, error)) {
 		c.val, c.err = fn()
 		normalError = true
 	}()
-
+	/** if fn panic then because we recover then code reach here but here normalError is still false.
+	  so we set the errorRecovered to inform that we already have panic error*/
 	if !normalError {
 		errorRecovered = true
 	}
