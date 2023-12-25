3451cc79eccff59946e388460dd1132b99997b84
amitiwary999 <tiwarya9@gmail.com>
2023-12-25 21:43:13 +0530

    add comment and test the multimap with different data type in key and value

--- multipartmap/multimap.go
+++ multipartmap/multimap.go
@@ -22,6 +22,8 @@ func findPartition[T comparable](key T, size int) int {
 
 func NewPartitionedMap[T comparable](size int) *partion[T] {
 	partions := make([]*mapData[T], 0, size)
+	/** traverse for 'size' number of times and create array with size 'size'. each item has a map with
+	key type any comparable and value interface{}. */
 	for i := 0; i < size; i++ {
 		m := make(map[T]interface{})
 		partions = append(partions, &mapData[T]{m})
@@ -29,12 +31,20 @@ func NewPartitionedMap[T comparable](size int) *partion[T] {
 	return &partion[T]{size: size, val: partions}
 }
 
+/*
+* get the possible partition number of a key, get the map from array using the partition number as index
+then set the value in that map
+*/
 func (p *partion[T]) Set(key T, val interface{}) {
 	partN := findPartition[T](key, p.size)
 	mp := p.val[partN]
 	mp.m[key] = val
 }
 
+/*
+* get the possible partition number of a key, get the map from array using the partition number as index
+then get the value of that key from map
+*/
 func (p *partion[T]) Get(key T) interface{} {
 	partN := findPartition[T](key, p.size)
 	fmt.Printf("part to read %d\n", partN)
