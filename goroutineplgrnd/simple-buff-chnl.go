61c3374500a2cc8097ba81df7775d33cf1629d19
amitiwary999 <tiwarya9@gmail.com>
2023-12-30 23:18:34 +0530

    wait for all operation to complete

--- goroutineplgrnd/simple-buff-chnl.go
+++ goroutineplgrnd/simple-buff-chnl.go
@@ -2,20 +2,28 @@ package gortntry
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type bufChn struct {
 	nameChn chan string
+	wg      sync.WaitGroup
+}
+
+func (b *bufChn) Wait() {
+	b.wg.Wait()
 }
 
 func (b *bufChn) done() {
 	<-b.nameChn
+	b.wg.Done()
 }
 
 func (b *bufChn) EntryFunc(str string) {
 	b.nameChn <- str
 	fmt.Printf("before the goroutine %s\n", str)
+	b.wg.Add(1)
 	go func() {
 		defer b.done()
 		time.Sleep(time.Duration(5 * time.Second))
