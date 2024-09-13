63ef958986552c1054f17ccc771c7854e5bda5c0
amitiwary999 <tiwarya9@gmail.com>
2024-09-13 18:55:13 +0530

    img to pdf converter test

--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ require (
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/miekg/dns v1.1.58 // indirect
 	github.com/oklog/ulid/v2 v2.1.0 // indirect
+	github.com/phpdave11/gofpdi v1.0.14-0.20211212211723-1f10f9844311 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/signintech/gopdf v0.26.2 // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
---
+++ imgtopdf/imgpdfconv.go
@@ -0,0 +1,50 @@
+package imgtopdf
+
+import (
+	// "fmt"
+	// "golibcustom/gokyeuerun"
+	"fmt"
+	"image"
+	"log"
+	"os"
+
+	"github.com/signintech/gopdf"
+	// heap "golibcustom/priorityqueue"
+)
+
+func getImageBytes(filepath string) []byte {
+	b, err := os.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func ImgToPdf() {
+	homeDir, _ := os.UserHomeDir()
+	tempFilePath1 := fmt.Sprintf("%v/%v", homeDir, "try1.png")
+	tempFilePath2 := fmt.Sprintf("%v/%v", homeDir, "try2.png")
+	file1, _ := os.Open(tempFilePath1)
+	//file2, _ := os.Open(tempFilePath2)
+	image1, _, _ := image.DecodeConfig(file1)
+	// image2, _, _ := image.DecodeConfig(file2)
+	file1.Close()
+
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: gopdf.Rect{W: 595.28, H: 841.89}}) //595.28, 841.89 = A4
+	pdf.AddPage()
+
+	//use path
+	pdf.Image(tempFilePath1, 0, 0, nil)
+	//use image holder by []byte
+	imgH1, err := gopdf.ImageHolderByBytes(getImageBytes(tempFilePath2))
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+	pdf.ImageByHolder(imgH1, 0, float64(image1.Height), nil)
+	fmt.Printf("next x %v y %v \n", pdf.GetX(), pdf.GetY())
+
+	pdf.WritePdf("image2.pdf")
+	fmt.Printf("final x %v y %v \n", pdf.GetX(), pdf.GetY())
+}
